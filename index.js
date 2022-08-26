aa185af855351c511b76e14e548fad679d1d15b7
ledong17 <ledong5012@gmail.com>
2022-08-27 00:05:40 +0900

    login 데이터 세션 전달 문제  해결

--- index.js
+++ index.js
@@ -4,7 +4,22 @@ const dotenv = require("dotenv").config();
 const path = require("path");
 const multer = require("multer");
 const cloudinary = require("cloudinary");
-const { count } = require("console");
+// session 처리
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
+
+app.use(
+  session({
+    secret: "비밀코드",
+    resave: true,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set("port", process.env.PORT || 8099);
 const PORT = app.get("port");
@@ -46,7 +61,7 @@ app.get("/join", (req, res) => {
   res.render("join", { title: "회원가입" });
 });
 
-app.get("/list", (req, res) => {
+app.get("/list", isLogged, (req, res) => {
   db.collection("movie_board")
     .find()
     .toArray((err, result) => {
@@ -69,6 +84,10 @@ app.get("/review/:id", (req, res) => {
   });
 });
 
+app.get("/success", (req, res) => {
+  res.render("registerSuccess", { title: "회원가입 완료!" });
+});
+
 app.post("/signUp", (req, res) => {
   const id = req.body.userID;
   const password = req.body.userPW;
@@ -86,10 +105,6 @@ app.post("/signUp", (req, res) => {
       res.redirect("/success");
     }
   );
-});
-
-app.get("/success", (req, res) => {
-  res.render("registerSuccess", { title: "회원가입 완료!" });
 });
 
 app.post("/register", fileUpload.single("image"), (req, res) => {
@@ -133,6 +148,71 @@ app.post("/register", fileUpload.single("image"), (req, res) => {
       }
     );
   });
+});
+
+function isLogged(req, res, next) {
+  if (req.user) {
+    next();
+  } else {
+    res.send(
+      `<script>alert("로그인이 필요합니다."); location.href="/"</script>`
+    );
+  }
+}
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "userID",
+      passwordField: "userPW",
+      session: true,
+      passReqToCallback: false,
+    },
+    (id, password, done) => {
+      console.log(id, "===", password);
+      db.collection("member").findOne({ userID: id }, (err, result) => {
+        if (err) {
+          console.log(err);
+          return done(err);
+        }
+        if (!result) {
+          console.log("result 없음");
+          return done(null, false, { message: "존재하지 않는 아이디 입니다." });
+        } else {
+          if (password === result.userPW) {
+            console.log("로그인 성공");
+            return done(null, result);
+          } else {
+            console.log("로그인 실패");
+            return done(null, false, { message: "password를 확인해주세요" });
+          }
+        }
+      });
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  console.log("serializeUser===", user);
+  done(null, user.userID);
+});
+passport.deserializeUser((id, done) => {
+  db.collection("movie_member").findOne({ id: id }, (err, result) => {
+    console.log("====", result);
+    done(null, result);
+  });
+});
+
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/",
+    successRedirect: "/list",
+  })
+);
+app.get("/aa", (req, res) => {
+  console.log(req.user);
+  res.send("dfdf");
 });
 
 app.listen(PORT, () => {
